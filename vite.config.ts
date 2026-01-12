ce0095d6e2d37deac5469640f61b0536d5c0877d
Debjitds <debjitchsarkarofficial2003@gmail.com>
2026-01-12 12:43:22 +0530

    fav config

--- vite.config.ts
+++ vite.config.ts
@@ -112,6 +112,18 @@ export default defineConfig(({ mode }) => ({
             type: "image/png",
             purpose: "maskable"
           },
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
         ],
         "screenshots": [
           {
